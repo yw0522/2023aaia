dc2c1a28c1723bdd208f73f7d72ed2fa5562f484
yw0522 <12750475@me.mcu.edu.tw>
2023-11-14 11:49:07 +0800

    week10

---
+++ week10/week10-6.py
@@ -0,0 +1,6 @@
+class Solution:
+    def average(self, salary: List[int]) -> float:
+        total=sum(salary)-max(salary)-min(salary)
+        N=len(salary)-2
+        return total/N
+        #return (sum(salary)-max(salary)-min(salary))/(len(salary)-2)
