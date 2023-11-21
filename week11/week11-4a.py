0319021a89a5d4cfad3509d47a8877ac48bd20b6
yw0522 <12750475@me.mcu.edu.tw>
2023-11-21 12:06:40 +0800

    week11

---
+++ week11/week11-4a.py
@@ -0,0 +1,10 @@
+x,y=list(map(int,input().split()))
+print('Enter two numbers: The remainder is' ,x%y)
+
+
+
+x,y=list(map(int,input().split()))
+if x==y:
+	print('Enter two numbers:  It is a square ', end='')
+else:
+	print('Enter two numbers:  It is not a square ', end='')
---
+++ week11/week11-5b.py
@@ -0,0 +1,5 @@
+a=int(input())
+if a-2>0:
+	print(a-2,end='')
+else:
+	pritn(2-1,end='')
